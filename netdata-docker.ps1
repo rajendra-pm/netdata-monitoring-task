a53c090090fd5cf25130cc1929d60c6702e85e0e
Rajendra P M <rajendrapm03@gmail.com>
2025-10-02 12:07:57 +0530

    Update netdata-docker.ps1

--- netdata-docker.ps1
+++ netdata-docker.ps1
@@ -12,4 +12,18 @@ docker ps
 # Open your browser:
 # - Local machine: http://localhost:19999
 # - VM/EC2: http://<Public-IP>:19999
+# Pull and run Netdata Docker container
+
+docker pull netdata/netdata
+
+# Run Netdata container
+docker run -d --name=netdata -p 19999:19999 --cap-add=SYS_PTRACE \
+  -v netdataconfig:/etc/netdata \
+  -v netdatalib:/var/lib/netdata \
+  -v netdatacache:/var/cache/netdata \
+  -v /etc/passwd:/host/etc/passwd:ro \
+  -v /etc/group:/host/etc/group:ro \
+  -v /proc:/host/proc:ro \
+  -v /sys:/host/sys:ro \
+  netdata/netdata
 
